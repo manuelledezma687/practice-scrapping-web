7cf6c8769008af6cde8ae4463946a6232269d3c7
manuelledezma687 <manuelledezma687@gmail.com>
2023-02-05 16:18:44 -0300

    scrappy project

--- news.py
+++ news.py
@@ -1,7 +1,7 @@
-import requests
-import lxml.html as html
 import os
 import datetime
+import requests
+from lxml import html
 
 
 HOME_URL = 'https://www.larepublica.co/'
@@ -10,24 +10,22 @@ XPATH_LINK_TO_ARTICLE = '//a[contains(@class,"kicker")]/@href'
 XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY ='//div[@class="html-content"]//text()'
 
+
 def get_title(link):
-    #separamos por "/" y nos quedamos con el ultimo que elemento 
+    #separamos por "/" y nos quedamos con el ultimo que elemento
     url = link.split('/')[-1]
     #separamos por "-" y eliminamos el ultimo elemento
     title_list=url.split('-')[:-1]
     #Unimos lo anterior
-    title = " ".join(title_list)
+    return " ".join(title_list)
 
-    return(title)
 
 def parse_notice(link, today):
     try:
-        response = requests.get(link)
+        response = requests.get(link, timeout=10)
         if response.status_code == 200:
-            
             notice = response.content.decode('utf-8')
             parsed = html.fromstring(notice)
-
             try:
                 title = get_title(link)
                 summary = parsed.xpath(XPATH_SUMMARY)[0]
@@ -35,25 +33,24 @@ def parse_notice(link, today):
             except IndexError:
                 print("as")
                 return
-            
-            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as f:
-                f.write(title)
-                f.write('\n\n')
-                f.write(summary)
-                f.write('\n\n')
-                for p in body:
-                    f.write(p)
-                    f.write('\n')
-            
+
+            with open(f'{today}/{title}.txt', 'w', encoding='utf-8') as file:
+                file.write(title)
+                file.write('\n\n')
+                file.write(summary)
+                file.write('\n\n')
+                for post in body:
+                    file.write(post)
+                    file.write('\n')
+
         else:
             raise ValueError(f'Error: {response.status_code}')
-    except ValueError as ve:
-        print(ve)
-
+    except ValueError as value_error:
+        print(value_error)
 
 def parse_home():
     try:
-        response = requests.get(HOME_URL)
+        response = requests.get(HOME_URL, timeout=10)
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
@@ -62,19 +59,19 @@ def parse_home():
             today = datetime.date.today().strftime('%d-%m-%Y')
             if not os.path.isdir(today):
                 os.mkdir(today)
-            
+
             for link in links_to_notices:
                 parse_notice(link, today)
 
         else:
             raise ValueError(f'Error: {response.status_code}')
-    except ValueError as ve:
-        print(ve)
+    except ValueError as value_error:
+        print(value_error)
 
 
 def run():
     parse_home()
-    
+
 
 if __name__ == "__main__":
     run()
--- project.py
+++ project.py
@@ -1,5 +1,5 @@
 import requests
-import lxml.html as html # para aplicar Xpath a HTML
+from lxml import html
 
 HOME_URL = 'https://www.larepublica.co/'
 
@@ -10,25 +10,24 @@ XPATH_SUMMARY = '//div[@class="lead"]/p/text()'
 XPATH_BODY = '//div[@class="html-content"]/p[not(@class)]/text()'
 
 
-
 def parse_home():
     try:
-        response = requests.get(HOME_URL)
-        
+        response = requests.get(HOME_URL,timeout=10)
+
         if response.status_code == 200:
             home = response.content.decode('utf-8')
             parsed = html.fromstring(home)
-    
+
             links_to_notices = parsed.xpath(XPATH_LINK_TO_ARTICLE)
             print(len(links_to_notices))
             print(links_to_notices)
-            
+
         else:
             raise ValueError(f"Error: {response.status_code}")
 
 
-    except ValueError as ve: 
-        print(ve)
+    except ValueError as value_error:
+        print(value_error)
 
 def main():
     parse_home()
